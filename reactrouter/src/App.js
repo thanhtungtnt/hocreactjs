35261c61652a516513f1a3ceb118dad081c6fc7c
tungpham.bh@gmail.com <tungpham.bh@gmail.com>
2020-12-19 15:53:33 +0700

    update code reactrouter

---
+++ reactrouter/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, 
+        Switch,
+        Route,
+        Link
+} from 'react-router-dom'
+import About from './Components/About.js';
+import Users from './Components/Users.js';
+import Home from './Components/Home.js';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+
+         
+
+          <Switch>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
