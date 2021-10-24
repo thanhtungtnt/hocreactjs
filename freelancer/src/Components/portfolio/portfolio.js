eea7e7953b00a966afbea4d3eb4e0687f2e61707
thanhtungtnt <tungpham.bh@gmail.com>
2021-10-24 23:07:17 +0700

    Update portfolio.js

--- freelancer/src/Components/portfolio/portfolio.js
+++ freelancer/src/Components/portfolio/portfolio.js
@@ -10,12 +10,12 @@ class Portfolio extends Component {
                 <div className="container">
                     <PortfolioHead />
                     <div className="row justify-content-center">
-                        <PortfolioItem img="cabin.png" modal="#portfolioModal1" />
-                        <PortfolioItem img="cake.png" modal="#portfolioModal2" />
-                        <PortfolioItem img="circus.png" modal="#portfolioModal3" />
-                        <PortfolioItem img="game.png" modal="#portfolioModal4" />
-                        <PortfolioItem img="safe.png" modal="#portfolioModal5" />
-                        <PortfolioItem img="submarine.png" modal="#portfolioModal6" />
+                        <PortfolioItem img="cabin.png" modal="#portfolioModal1" title="Hồ sơ Nguyễn Ngọc" />
+                        <PortfolioItem img="cake.png" modal="#portfolioModal2" title="Nguyễn Văn Toàn" />
+                        <PortfolioItem img="circus.png" modal="#portfolioModal3" title="Văn Thanh" />
+                        <PortfolioItem img="game.png" modal="#portfolioModal4" title="Quang Hải" />
+                        <PortfolioItem img="safe.png" modal="#portfolioModal5" title="Trần Đình Trọng" />
+                        <PortfolioItem img="submarine.png" modal="#portfolioModal6" title="Đỗ Huy Hoàng" />
                     </div>
                 </div>
             </section>
